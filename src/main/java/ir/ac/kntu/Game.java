9ff7924a8891e645cb8753a9a8be2a799e2d324a
alireza <alisnz7779@gmail.com>
2023-06-27 17:56:05 +0330

    NormalTank is almost ready,image is ready

--- src/main/java/ir/ac/kntu/Game.java
+++ src/main/java/ir/ac/kntu/Game.java
@@ -1,8 +1,10 @@
 package ir.ac.kntu;
 
+import ir.ac.kntu.Constants.GlobalConstants;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -10,21 +12,18 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-public class Game extends Application {
-    public void start(Stage stage) throws Exception {
-        stage.setTitle("Pong-Game");
-        Canvas canvas = new Canvas(CANVAS_WIDTH, GlobalConstants.CANVAS_HEIGHT);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        Timeline tl = new Timeline(new KeyFrame(Duration.millis(10),
-                e -> start(gc)));
-        tl.setCycleCount(Timeline.INDEFINITE);
-        Scene scene = new Scene(new Pane(canvas));
-        canvas.setOnMouseClicked(e -> gameState = GameState.RUNNING);
-        //TODO attach EventHandler
+import static ir.ac.kntu.Constants.GlobalConstants.CANVAS_HEIGHT;
+import static ir.ac.kntu.Constants.GlobalConstants.CANVAS_WIDTH;
 
+public class Game extends Application {
+    @Override
+    public void start(Stage stage) throws Exception{
+        Canvas canvas=new Canvas(CANVAS_WIDTH,CANVAS_HEIGHT);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        Group root=new Group(canvas);
+        Scene scene=new Scene(root);
         stage.setScene(scene);
         stage.show();
-        tl.play();
     }
 
 }
--- src/main/java/ir/ac/kntu/GameObject/Tank/NormalTank.java
+++ src/main/java/ir/ac/kntu/GameObject/Tank/NormalTank.java
@@ -2,14 +2,31 @@ package ir.ac.kntu.GameObject.Tank;
 
 import ir.ac.kntu.Constants.Direction;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 
-public class NormalTank extends Tank{
+import java.io.File;
+
+public class NormalTank extends Tank {
     public NormalTank(double xPos, double yPos, int health, Direction direction, int firePower) {
         super(xPos, yPos, health, direction, firePower);
     }
 
     @Override
     public void draw(GraphicsContext gc) {
+    gc.drawImage(imageLoader(),getxPos(),getyPos(),35,35);
+    }
+
+    public Image imageLoader() {
+        String address = null;
+        switch (getDirection()) {
+            case UP -> address = "object_8.png";
+            case DOWN -> address = "object_10.png";
+            case LEFT -> address = "object_12.png";
+            case RIGHT -> address = "object_14.png";
+        }
+        File file=new File(address);
+        String address2=file.toURI().toString();
+        return  new javafx.scene.image.Image(address2);
 
     }
 }
